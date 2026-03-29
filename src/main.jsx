77c1c1e3ad87acecc6dfedb53c307430fdc01a2e
islanddave <islanddave@gmail.com>
2026-03-29 09:48:41 -0400

    Add hostname router for caltasks.futureishere.net

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import KinkGuide from './kink-guide.jsx'
+import Caltasks from './caltasks.jsx'
+
+const ROUTES = {
+  'caltasks.futureishere.net': { component: Caltasks, title: "Jen's Cowork Setup" },
+}
+
+const host = window.location.hostname
+const route = ROUTES[host]
+const App = route ? route.component : KinkGuide
+
+if (route) { document.title = route.title }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <KinkGuide />
-  </React.StrictMode>
+  <React.StrictMode><App /></React.StrictMode>
 )
 
-// Register service worker for PWA / offline support
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').catch(() => {})
