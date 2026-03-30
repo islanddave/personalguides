d104e8c2025639ec17b80d25662c3367390789b1
islanddave <islanddave@gmail.com>
2026-03-30 13:39:03 -0400

    add jen-morning + jen-envsetup routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import KinkGuide from './kink-guide.jsx'
 import Caltasks from './caltasks.jsx'
 import MorningDashboard from './morning-dashboard.jsx'
+import JenMorningDashboard from './jen-morning-dashboard.jsx'
+import JenEnvSetup from './jen-envsetup.jsx'
 
 const ROUTES = {
   'caltasks.futureishere.net': { component: Caltasks, title: "Jen's Cowork Setup" },
   'morning.futureishere.net': { component: MorningDashboard, title: 'Morning Dashboard' },
+  'jen-morning.futureishere.net': { component: JenMorningDashboard, title: "Jen's Morning Dashboard" },
+  'jen-envsetup.futureishere.net': { component: JenEnvSetup, title: "Jen's Cowork Setup Guide" },
 }
 
 const host = window.location.hostname
